3346011fb3f0dc408e5b1eab9ee267ab2fdf60c2
Bryan Heu <teamriab@gmail.com>
2014-03-20 10:27:02 -0700

    change uicorn log path

--- config/unicorn.sh
+++ config/unicorn.sh
@@ -1,8 +1,8 @@
 root = "/home/deployer/apps/cap2r3/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.stderr.log"
-stdout_path "#{root}/log/unicorn.stdout.log"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.cap2r3.sock"
 worker_processes 2
