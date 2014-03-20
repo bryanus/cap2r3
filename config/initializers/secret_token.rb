c82d5be4d3fc4395366a6a7ff138ee14f162d03c
Bryan Heu <teamriab@gmail.com>
2014-03-20 09:40:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cap2r3::Application.config.secret_key_base = '4f514a4bb993a059af2684cf249eece5dce26ad06cefddc14e627fab1a9b4d29a20e5cc5385825dd3c5ae79c202afafd0285612e5f166224584ad5389021300c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cap2r3::Application.config.session_store :cookie_store, key: '_cap2r3_session'
