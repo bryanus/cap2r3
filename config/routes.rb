4e104e673f49fcb82f03f6154923fb0f076431aa
Bryan Heu <teamriab@gmail.com>
2014-03-20 10:00:28 -0700

    fix route to post index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Cap2r3::Application.routes.draw do
   resources :posts
 
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
